7a057ca55e536b23e2383b771fec3ade6ba80009
Greg Wanner <greg@tegogroup.com>
2024-02-20 12:29:28 -0500

    added print number between 1000 and -1000

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -4,6 +4,14 @@
     {
         //LukeWarm Section: Create methods below
         //Write a method that will print to the console all numbers 1000 through - 1000
+        public static void PrintNumberRange()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
 
         //Write a method that will print to the console numbers 3 through 999 by 3 each time
 
@@ -27,7 +35,7 @@
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+            PrintNumberRange();
         }
     }
 }
